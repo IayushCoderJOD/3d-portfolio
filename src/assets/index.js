c12cf7471b82242bc1dc728580bb4ce493b67468
ayush tyagi <ayushtyagi4810poco@gmail.com>
2023-12-16 17:42:06 +0530

    updated one single project in portfolio

---
+++ src/assets/index.js
@@ -0,0 +1,84 @@
+import logo from "./logo.svg";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import gfg from "./gfg.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import crud from "./crud.png";
+
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+import victory from "./company/victory.png"
+import css from "./tech/css.png";
+import docker from "./tech/docker.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import cbr from "./cbr.png";
+import freelance from "./freelance.png";
+
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import threejs from "./tech/threejs.svg";
+import gfgWeb from "./gfgWeb.png";
+import blood from "./blood.png";
+import netflix from './netflix.png'
+import snake from "./snake.png";
+
+
+// import gfg from "./company/gfg.png"
+import meta from "./company/meta.png";
+import tsf from "./company/tsf.png"
+import shopify from "./company/shopify.png";
+import starbucks from "./company/starbucks.png";
+import tesla from "./company/tesla.png";
+
+import carrent from "./carrent.png";
+import jobit from "./jobit.png";
+import tripguide from "./tripguide.png";
+
+export {
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  docker,
+  crud,
+  figma,
+  git,
+  html,
+  netflix,
+  cbr,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  gfg,
+  tailwind,freelance,
+  typescript,
+  threejs,
+  meta,
+  tsf,
+  victory,
+  shopify,
+  starbucks,
+  tesla,
+  gfgWeb,
+  blood,
+  snake,
+  carrent,
+  jobit,
+  tripguide,
+};
